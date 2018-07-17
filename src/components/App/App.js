f7831a122b90a39fe46d992f9e37738309819dea
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-07-17 10:08:52 +0300

    feat: added(changed) the App component in own folder and to use BusinessList and SearchBar components. The app works in a static mode: the businesses shown are hard coded for now, and not fetched from YELP api yet

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import SearchBar from "../SearchBar/SearchBar";
+import BusinessList from "../BusinessList/BusinessList";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
